9069f8cad6a8b58ae3f2ca39e615b5d83104ef3c
Omri Armstrong <t-omriar@microsoft.com>
2017-06-08 17:11:52 +0300

    moving tile embed to js sdk

---
+++ App Owns Data/PowerBIEmbedded_AppOwnsData/Models/TileEmbedConfig.cs
@@ -0,0 +1,10 @@
+using Microsoft.PowerBI.Api.V2.Models;
+using System;
+
+namespace PowerBIEmbedded_AppOwnsData.Models
+{
+    public class TileEmbedConfig : EmbedConfig
+    {
+        public string dashboardId { get; set; }
+    }
+}
