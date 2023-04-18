06c0915b0da518d65f6d92c485767e698601c444
Jerry Tang <36253164+secgoal@users.noreply.github.com>
2023-04-18 17:18:06 +0800

    Update Export-PowerBIReport.ps1
    update as per Amos's suggestion

--- PowerShell Scripts/Export-PowerBIReport.ps1
+++ PowerShell Scripts/Export-PowerBIReport.ps1
@@ -1,10 +1,14 @@
 ########################################################################
 # Script created by Jerry Tang
-# 30/03/2033
+# 30/03/2023
 #
-# **Notes**
-# Make sure you have the PowerBIMgmt module installed
-# Install-Module -Name MicrosoftPowerBIMgmt
+# Instructions:
+# 1. Install PowerShell (https://msdn.microsoft.com/en-us/powershell/scripting/setup/installing-windows-powershell) 
+#    and the Power BI PowerShell cmdlets (Install-Module MicrosoftPowerBIMgmt)
+#    https://docs.microsoft.com/en-us/powershell/power-bi/overview?view=powerbi-ps
+# 2. Run PowerShell as an administrator
+# 3. Fill in the parameters below
+# 4. Change PowerShell directory to where this script is saved
 #
 # REST API: Reports - Export Report In Group https://learn.microsoft.com/en-us/rest/api/power-bi/reports/export-report-in-group
 # If the file is large so the export takes long time to complete, then we might reach the timeout (can happen not just in Try-It). 
@@ -16,12 +20,15 @@ Connect-PowerBIServiceAccount
 
 $workspaceName = "<WorkspaceName>"
 $workspace = Get-PowerBIWorkspace -Name $workspaceName
+$reportId = "<ReportId>"
+
 if($workspace) {
-  Write-Host "The workspace named $workspaceName already exists"
+  Write-Host "The workspace named $workspaceName already exists."
+  
+  #https://learn.microsoft.com/en-us/powershell/module/microsoftpowerbimgmt.reports/export-powerbireport?view=powerbi-ps
+  Export-PowerBIReport -Id $reportId -WorkspaceId $workspace.Id -OutFile .\ExportReportByPowershell.pbix -Verbose
 }
 else {
-  Write-Host "The workspace named $workspaceName does not exist"
+  Write-Host "The workspace named $workspaceName does not exist."
+  return
 }
-
-#https://learn.microsoft.com/en-us/powershell/module/microsoftpowerbimgmt.reports/export-powerbireport?view=powerbi-ps
-Export-PowerBIReport -Id '<ReportID>' -WorkspaceId $workspace.Id -OutFile .\ExportReportByPowershell.pbix -Verbose
